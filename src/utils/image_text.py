2f55b88ba4d391c1f55c8d8980ced8aa0dc99ff1
Sam22G <samuelpg03@gmail.com>
2025-02-28 18:05:23 -0600

    Remoción de elementos de configuración

--- src/utils/image_text.py
+++ src/utils/image_text.py
@@ -1,15 +1,7 @@
 from pygame import Surface
 from pygame.font import SysFont
 
-def draw_text(text : str, font : SysFont, color : tuple, outline_color : tuple , x :int, y : int, screen : Surface):
-    """Dibuja texto con contorno en Pygame usando pygame.freetype"""
-    border_size = 2  # Grosor del contorno
 
-    # Dibuja el contorno en 4 direcciones
-    font.render_to(screen, (x - border_size, y), text, outline_color)
-    font.render_to(screen, (x + border_size, y), text, outline_color)
-    font.render_to(screen, (x, y - border_size), text, outline_color)
-    font.render_to(screen, (x, y + border_size), text, outline_color)
-
-    # Dibuja el texto principal encima
-    font.render_to(screen, (x, y), text, color)
+def draw_text(text : str, font : SysFont, color : tuple, x : int, y : int, screen : Surface):
+    img = font.render(text, True, color)
+    screen.blit(img, (x, y))
